830cc7ee9fa10cf251a9ac2ffe9fafc1c22500b3
Javi96 <josejaco@ucm.es>
2018-05-20 17:18:09 +0200

    Link de mueve en diagonal

--- src/link.js
+++ src/link.js
@@ -14,16 +14,24 @@ function loadLink(Q){
         //this.on("enemy.hit","enemyHit");
         },
         step:function(dt){
-        	if(Q.inputs['up']){
-	        	this.play("walking_up");
-	        }else if(Q.inputs['down']){
-	        	this.play("walking_down");
-	        }else if(Q.inputs['right']){
-	        	this.play("walking_right");
-	        }else if(Q.inputs['left']){
-				this.play("walking_left");
-	        }else{
 
+
+        	if(Q.inputs['up'] && !Q.inputs['left'] && !Q.inputs['right']){
+	        	this.play("walking_up");
+	        }else if(Q.inputs['down'] && !Q.inputs['left'] && !Q.inputs['right']){
+	        	this.play("walking_down");
+	        }else if(Q.inputs['right'] && !Q.inputs['up'] && !Q.inputs['down']){
+	        	this.play("walking_right");
+	        }else if(Q.inputs['left'] && !Q.inputs['up'] && !Q.inputs['down']){
+				this.play("walking_left");
+	        }else if(Q.inputs['up'] && Q.inputs['right']){
+	        	this.play("walking_up_right");
+	        }else if(Q.inputs['up'] && Q.inputs['left']){
+	        	this.play("walking_up_left");
+	        }else if(Q.inputs['down'] && Q.inputs['right']){
+	        	this.play("walking_down_right");
+	        }else if(Q.inputs['down'] && Q.inputs['left']){
+	        	this.play("walking_down_left");
 	        }
             
             
@@ -35,13 +43,21 @@ function loadLink(Q){
 		"stand_right": { frames: [0] },
 
 		"walking_up": { frames: [11,12,13,14,15,16,17,18], rate: 1/16, next: 'stand_up'},
+		"walking_up_right": { frames: [8,9,10], rate: 1/12, next: 'stand_up_right' },
+		"walking_up_left": { frames: [19,20,21], rate: 1/12, next: 'stand_up_left' },
 		"stand_up": { frames: [11] },
+		"stand_up_right": { frames: [8] },
+		"stand_up_left": { frames: [19] },
 
 		"walking_down": { frames: [44,45,46,47,48,49,50,51], rate: 1/16, next: 'stand_down' },
+		"walking_down_right": { frames: [52,53,54], rate: 1/12, next: 'stand_down_right' },
+		"walking_down_left": { frames: [41,42,43], rate: 1/12, next: 'stand_down_left' },
 		"stand_down": { frames: [44] },
+		"stand_down_right": { frames: [52] },
+		"stand_down_left": { frames: [41] },
 
 		"walking_left": { frames: [33,34,35,36,37,38,39,40], rate: 1/16, next: 'stand_left' },
 		"stand_left": { frames: [33] }
- 
+
       });
 }
