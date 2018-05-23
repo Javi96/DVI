dc9fbf6742d98e0eb233e63c456633474d7f951f
Manuel Hidalgo Lorente <36233273+ManuMinue@users.noreply.github.com>
2018-05-23 11:45:09 +0200

    Se ajustan los mapas

--- data/Link.tsx
+++ data/Link.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Link" tilewidth="16" tileheight="24" tilecount="44" columns="11">
-    <image source="../images/Link.png" width="176" height="96" />
+ <image source="../images/Link.png" width="176" height="96"/>
+ <tile id="0">
+  <properties>
+   <property name="Class" value="Player"/>
+  </properties>
+ </tile>
 </tileset>
