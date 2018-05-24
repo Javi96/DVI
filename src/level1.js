bb317c462651d55558bccebd910ad7e655190d14
Manuel Hidalgo Lorente <36233273+ManuMinue@users.noreply.github.com>
2018-05-24 17:24:38 +0200

    Se pone a ganon en el mapa y se introducen sonidos MP3

--- src/level1.js
+++ src/level1.js
@@ -6,18 +6,16 @@ function loadLevel1(Q) {
 
         Q.stageTMX('casa_link_map.tmx', stage);
 
-        stage.insert(new Q.Ganon());
-
-        var player = Q("Player").first();
+        var player = Q('Player').first();
         var sword = stage.insert(new Q.Sword(), player);
 
-        stage.add("viewport").follow(player, { x: true, y: true }, {
+        stage.add('viewport').follow(player, { x: true, y: true }, {
             minY: 0,
             maxY: 512,
             minX: 0,
             maxX: 1024,
         });
-        Q.audio.play('forest.mp3', {loop:true});
+        Q.audio.play('forest.mp3', { loop: true });
 
-    })
+    });
 }
