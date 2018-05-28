c45bb20d75903fecf11520875881b7fe1ce34cd9
Lidiacon <lidiacon@ucm.es>
2018-05-28 14:51:14 +0200

    Add files via upload

---
+++ data/castle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="castle" tilewidth="8" tileheight="8" tilecount="2162" columns="46">
+ <image source="../images/castle.png" width="369" height="378"/>
+</tileset>
