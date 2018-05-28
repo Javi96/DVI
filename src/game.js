45dceddad4ae5c6bfb54b573f122a1acdc5d028c
Lidiacon <lidiacon@ucm.es>
2018-05-28 18:41:22 +0200

    Arreglados corazones (ahora sí)

--- src/game.js
+++ src/game.js
@@ -50,6 +50,9 @@ window.addEventListener('load', function() {
         var hearts = [];
         for(let i = 0; i < 3; i++){
             hearts[i] = new Q.Heart({x: 20 + i * 20, num: i});
+            if(Q.state.get("maxLives") > Q.state.get("lives") && i >= Q.state.get("lives")){
+                hearts[i].p.frame = 2;
+            }
             stage.insert(hearts[i]);
         }
         stage.insert(new Q.Rupee());
