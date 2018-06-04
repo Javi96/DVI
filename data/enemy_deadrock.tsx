1000272af2ec27b93e4c17e8206b1707d2a0feb5
Manuel Hidalgo Lorente <36233273+ManuMinue@users.noreply.github.com>
2018-06-04 13:25:31 +0200

    Se añaden comentarios y se procede a comenzar con el enemigo "deadrock"

---
+++ data/enemy_deadrock.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="enemy_deadrock" tilewidth="16" tileheight="16" tilecount="4" columns="4">
+ <image source="../images/enemy_deadrock.png" width="64" height="16"/>
+ <tile id="0">
+  <properties>
+   <property name="Class" value="Deadrock"/>
+  </properties>
+ </tile>
+</tileset>
--- src/game.js
+++ src/game.js
@@ -42,7 +42,7 @@ window.addEventListener('load', function() {
     var maps = 'house_link.tmx, house_link_forest.tmx, castle_map.tmx';
 
     var link = 'link.png, link.json, loading.sword3.png, loading.sword3.json, sword.sword3.png, sword.sword3.json, dying.png, dying.json';
-    var enemies = 'enemy_ganon.png, enemy_ganon.json';
+    var enemies = 'enemy_ganon.png, enemy_ganon.json, enemy_deadrock.png, enemy_deadrock.json';
     var items = 'item_lamp.png, item_chest.png, item_chest.json';
     var UI = 'hearts.png, heart.json, dialog_box.png, rupee_icon.png';
     var sounds = 'forest.mp3, sword1.mp3, chest_open.mp3, picked_item.mp3, title_screen.mp3, hero_dying.mp3, hero_hurt.mp3, heart.mp3, game_over.mp3, rupee_counter.mp3';
@@ -63,7 +63,9 @@ window.addEventListener('load', function() {
         Q.compileSheets('link.png', 'link.json');
         Q.compileSheets('loading.sword3.png', 'loading.sword3.json');
         Q.compileSheets('sword.sword3.png', 'sword.sword3.json');
+
         Q.compileSheets('enemy_ganon.png', 'enemy_ganon.json');
+        Q.compileSheets('enemy_deadrock.png', 'enemy_deadrock.json');
 
         Q.compileSheets('item_chest.png', 'item_chest.json');
         Q.compileSheets('hearts.png', 'heart.json');
