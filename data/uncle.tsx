bf0b58eb10e6d4e51752a0e0a829c05e42eff7ea
Manuel Hidalgo Lorente <36233273+ManuMinue@users.noreply.github.com>
2018-06-08 13:25:20 +0200

    minor bug

--- data/uncle.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="uncle" tilewidth="28" tileheight="28" tilecount="1" columns="1">
- <image source="../images/uncle.png" width="28" height="28"/>
- <tile id="0">
-  <properties>
-   <property name="Class" value="Uncle"/>
-  </properties>
- </tile>
-</tileset>
