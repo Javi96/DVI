9fbb43cbf2537cab29650e63eef72e0045c94e2f
pedagova <permuta.yt@gmail.com>
2018-06-10 15:12:09 +0200

    añadido comportamiento final de gannon

--- src/enemies/ganon.js
+++ src/enemies/ganon.js
@@ -7,17 +7,49 @@ Quintus.Ganon = function(Q) {
                 sheet: 'ganonWalk',
                 sprite: 'ganonAnim', 
                 score: 100,
-                hp: 3
+                hp: 1,
+                invokeFireTime: 0,
+                hideousLaughter:false,
             });
             this.add('defaultEnemy');
+            
+            this.on('kicked', this, 'kicked');
+            this.on('dead', this, 'dead');
         },
         step: function(dt) {
             this.p.invicible -= dt;
             if (!this.p.dead) {
                 this.play('stand');
             }
-            if(this.p.hp==2 && lock){
-                lock = false;
+            if(this.p.hideousLaughter){
+                this.p.invokeFireTime = 1;
+                if(!Q('Deadrock').first() && !Q('Soldier').first()){
+                    this.p.hideousLaughter = false;
+                    this.p.y = 300
+                }
+                
+            }else{
+                if(this.p.invokeFireTime <= 0){
+                    this.p.invokeFireTime = 1;
+                    this.stage.insert(new Q.fire({x:this.p.x, y:this.p.y}))
+                }else{
+                    this.p.invokeFireTime -= dt;
+                }
+            }
+
+        },
+
+        destroyEffect: function(){
+            fire = Q('fire')   
+            for(f of fire.items){
+                f.destroy();
+            }
+        },
+
+        kicked: function(){        
+            if(!this.p.hideousLaughter)
+            {
+                this.p.hideousLaughter = true;
                 this.p.y=100;
                 this.stage.insert(new Q.Deadrock({x:150,y:250}));
                 this.stage.insert(new Q.Soldier({x:200,y:180}));
@@ -25,8 +57,14 @@ Quintus.Ganon = function(Q) {
                 this.stage.insert(new Q.Deadrock({x:325,y:250}));
                 this.stage.insert(new Q.Deadrock({x:150,y:400}));
                 this.stage.insert(new Q.Deadrock({x:325,y:400}));
+                fire = Q('fire')
+                console.log(fire);
+                for(f of fire.items){
+                    f.destroy();
+                }
             }
         }
+
     });
 
     Q.animations('ganonAnim', {
